aacc41d8e6e9a8739a6678e4917eb8bab09dcf26
Gerhard Popp <gerhrad@poppgerhard.at>
2022-02-25 18:43:42 +0100

    initial commit

---
+++ resources/views/pages/comming-soon.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+<div class="flex flex-col w-full min-h-screen justify-center items-center">
+    <img src="{{ asset('images/IMMohilft_Logo_black.png') }}" class="w-48 h-auto"/>
+    <h2>Cooming soon!</h2>
+</div>
+</x-app-layout>
