48c9d0689a28ab7b7bafd40ea5d97d6da8739304
Gerhard Popp <gerhrad@poppgerhard.at>
2022-02-26 17:14:35 +0100

    Formular V1

--- resources/views/livewire/donate-space-form.blade.php
+++ resources/views/livewire/donate-space-form.blade.php
@@ -105,6 +105,14 @@
         </div>
 
 
-        <button type="submit" class="mt-10 shadow-xl hover:shadow-lg focus:shadow-lg flex w-full justify-center bg-flag-yellow text-white font-bold text-xl py-3">{{ __('submit') }}</button>
+        <button type="submit" class="mt-10 shadow-xl hover:shadow-lg focus:shadow-lg flex w-full justify-center bg-flag-yellow text-white font-bold text-xl py-3 flex items-center space-x-3">
+        <span wire:loading wire:target="submit">
+            <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+        </span>
+            <span>{{ __('submit') }}</span>
+        </button>
     </form>
 </div>
