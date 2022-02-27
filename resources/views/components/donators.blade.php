9e1edf9352ca657604b54060ce803ed99e8c6a03
Gerhard Popp <gerhrad@poppgerhard.at>
2022-02-27 16:51:47 +0100

    wip

--- resources/views/components/donators.blade.php
+++ resources/views/components/donators.blade.php
@@ -1,16 +1,17 @@
 <div>
+    @if(count($donators))
+        <hr class="my-10 py-10 text-gray-900">
 
-    <hr class="my-10 py-10 text-gray-900">
 
+        <div class="content">
 
-    <div class="content">
+            <h5>Spenderliste:</h5>
 
-        <h5>Spenderliste:</h5>
-
-        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4">
-            @foreach($donators as $key => $donator)
-                <div class="p-3 border-b border-gray-400">{{ $donator->name }}</div>
-            @endforeach
+            <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4">
+                @foreach($donators as $key => $donator)
+                    <div class="p-3 border-b border-gray-400">{{ $donator->name }}</div>
+                @endforeach
+            </div>
         </div>
-    </div>
+    @endif
 </div>
