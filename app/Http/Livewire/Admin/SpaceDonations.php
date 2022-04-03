23db61ea30e56f55e2393d2d73cf37b358c7c26e
Gerhard Popp <gerhrad@poppgerhard.at>
2022-04-03 18:51:06 +0200

    update form

--- app/Http/Livewire/Admin/SpaceDonations.php
+++ app/Http/Livewire/Admin/SpaceDonations.php
@@ -61,8 +61,31 @@ class SpaceDonations extends Component {
             die( 'Error opening the file ' . $filename );
         }
 
+        $headers = [
+          'id',
+          'name',
+          'email',
+          'company',
+          'phone',
+          'address',
+          'zip',
+          'city',
+          'country',
+          'sqm',
+          'rooms',
+          'exklusiv',
+          'freerent',
+          'furnitured',
+          'available_from',
+          'available_to',
+          'descritpion',
+          'deleted_at',
+          'created_at',
+          'updated_at'
+        ];
+
 // write each row at a time to a file
-        foreach ( $this->spaces->toArray() as $row ) {
+        foreach ( array_merge($headers, $this->spaces->toArray()) as $row ) {
             $row = array_map( "utf8_decode", $row );
             fputcsv( $f, $row, ';' );
         }
